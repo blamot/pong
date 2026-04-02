2edc3d8afe65688b383f3402802d04b83587fe8f
Bart Lamot <bart.lamot@gmail.com>
2026-04-02 09:30:25 +0200

    Change default server port to 3001.

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Pong server running on http://localhost:${PORT}`);
 });
